3d76321a2b8de372f027c08914fc268e2a474450
daemon <daemon@mksinst.com>
2014-10-27 17:49:52 -0400

    Almost working, needs lots of refactoring
    Method for parsing the dir and re-naming is working, but the code needs
     a lot of work. Need to re-factor the except block, and move the complex
      functions out of iterate over list

---
+++ app/CreateIndex.py
@@ -0,0 +1,5 @@
+__author__ = 'foxma'
+
+import app
+
+app.create_path_index(["C:\Avanti"], "test_index.txt")
---
+++ app/tests/test_Iterate.py
@@ -0,0 +1,11 @@
+__author__ = 'foxma'
+
+from app.Search import *
+
+data = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/indexes/ItemNewRevW_O_sftwr.txt")
+# path = os.path.normpath("C:/AVANTI/")
+error_log = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/results/error_log.txt")
+results_log = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/results/results_log.txt")
+dstdir = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/results")
+index_file = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/indexes/local_att_delim.txt")
+print(iterate_over_list_create_objects(data, error_log, dstdir, index_file, results_log))
