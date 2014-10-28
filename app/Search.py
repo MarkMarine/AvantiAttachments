cddf1f1165f803ce57472f7217129bb5cfc497c1
daemon <daemon@mksinst.com>
2014-10-28 10:48:37 -0400

    Moved debug to separate commands

--- app/Search.py
+++ app/Search.py
@@ -6,6 +6,8 @@ import csv
 from app.timer import Timer
 
 
+
+
 # region Description of Functions
 # Load both text files into memory
 
@@ -33,6 +35,9 @@ from app.timer import Timer
 # endregion
 
 
+# debug_log = "results/debug_log.txt"  # TODO move this variable out of file
+
+
 def write_log(item, log, error=""):
     with open(log, "a+") as f:
         if len(error) > 0:
@@ -41,25 +46,40 @@ def write_log(item, log, error=""):
             f.write("%s\n" % str(item))
 
 
-def find_rev(searchtext, errorlog):  # This searches a string for the revision character
+def find_rev(searchtext, log, debug=False, debug_log=""):  # This searches a string for the revision character
     rev_keyword = re.compile("(rev|rev |rev-|rev_|rev\.|rev\. )([a-z0-9]{1,3})", re.IGNORECASE)
     # exclude_ext = re.compile("(pdf|png|xls|doc)", re.IGNORECASE)
     exclude_ext = ["pdf", "PDF", "png", "PNG", "xls", "doc", "DOC", "iew", "ise", "isi"]
     rev_twice = re.compile("(rev).*(rev)", re.IGNORECASE)
     if rev_twice.search(searchtext) is not None:
-        write_log(searchtext, errorlog, "MULTIPLE REVS IN NAME ERROR")
+        write_log(searchtext, log, "MULTIPLE REVS IN NAME ERROR")
     elif rev_keyword.search(searchtext) is not None:
+        if debug:
+            write_log("%s\trev_keyword.search is:\t%s\tnot None" %
+                      (searchtext, rev_keyword.search(searchtext)), debug_log)
         exclude = False
         for x in exclude_ext:
             if rev_keyword.search(searchtext).group(2) == x:  # TODO fix exclude list search
+                if debug:
+                    write_log("%s\tif rev_keyword:\t%s\t==\t%s\tis True, exclude" %
+                              (searchtext, rev_keyword.search(searchtext).group(2), x), debug_log)
                 exclude = True
         if not exclude:
+            if debug:
+                write_log("%s\tNot Exclude, so return\t%s" %
+                          (searchtext, rev_keyword.search(searchtext).group(2)), debug_log)
             return rev_keyword.search(searchtext).group(2)
         else:
+            if debug:
+                write_log("else return None:\t%s\t", debug_log)
             return None
     else:
+        if debug:
+            write_log("else PASS\tvalue:\t%s" % searchtext, debug_log)
         pass  # TODO make sure pass is appropriate here
 
+
+# TODO refactor find_rev to be simple
 
 def split_rev_table_data(line):
     # This splits info from the rev table file, a tab delimited text file with (PrtNum)\t(NewRev)\t(ECO for NewRev)\n
@@ -81,7 +101,8 @@ def create_new_bom_name(x):
 
 def is_a_bom_redline(x):
     bom_keyword = re.compile("([\W_]BOM[\W_])", re.IGNORECASE)  # finds bom separated by a non-word or "_" char
-    if bom_keyword.search(x) is not None:
+    redline_keyword = re.compile("RED LINE", re.IGNORECASE)
+    if bom_keyword.search(x) is not None or redline_keyword.search(x) is not None:
         return True
     else:
         return False
@@ -93,6 +114,33 @@ def is_material_spec(x):
         return True
     else:
         return False
+
+
+def is_item_attachment(num, file, eco, rev, root, errorlog, debug=False, debug_log=""):
+    if num in file:
+        if debug:
+            write_log("TRUE NUM:\t%s\tIN FILE:\t%s\tSEARCH REV:\t%s" % (num, file, rev), debug_log)
+        if eco in root:
+            if debug:
+                write_log("TRUE ECO:\t%s\tIN ROOT:\t%s\tSEARCH REV:\t%s" % (eco, root, rev), debug_log)
+            if not is_material_spec(file):
+                if debug:
+                    write_log("TRUE is not MS:\t%s\tSEARCH REV:\t%s" % (file, rev), debug_log)
+                if not is_a_bom_redline(file):
+                    if debug:
+                        write_log("TRUE is not BOM Redline:\t%s\tSEARCH REV:\t%s" % (file, rev), debug_log)
+                    if find_rev(file, errorlog, debug, debug_log) == filter_zeros_from_rev(rev):
+                        if debug:
+                            write_log("TRUE find_rev:\t%s\t== rev:%s\t%s" %
+                                      (file, rev, filter_zeros_from_rev(rev)), debug_log)
+                            write_log("is_item_attachment:\tnum: %s\tfile: %s\teco: %s\trev: %s\troot: %s" %
+                                      (num, file, eco, rev, root), debug_log)
+                        return True
+    else:
+        return False
+
+
+# TODO instrument and refactor if needed
 
 
 def filter_zeros_from_rev(rev):
@@ -107,7 +155,59 @@ def is_new_rev_folder(directory):
         return False
 
 
-def iterate_over_list_create_objects(data, errorlog, dstdir, index_file, result_log, debug=False):
+def perform_copy(num, rev, eco, root, file, line, dstdir, result_log):
+    src_file = os.path.join(root, file)
+    src_file_name, src_file_extension = os.path.splitext(src_file)
+    if src_file_extension == ".pdf":
+        # right now I only want to get pdf files because that is the data we have
+        # TODO pull the hardcode .pdf extension out of here and make it selectable
+        new_file_name = create_new_part_name(line) + src_file_extension  # get a new name
+        new_dst_file_name = os.path.join(dstdir, new_file_name)
+        if not os.path.exists(new_dst_file_name):
+            shutil.copy(src_file, new_dst_file_name)
+            write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
+                       (num, rev, eco, src_file, new_file_name)), result_log)
+        else:
+            ii = 1
+            while True:
+                new_name = os.path.join(create_new_part_name(line) + "(" + str(ii) + ")"
+                                        + src_file_extension)
+                new_name_path = os.path.join(dstdir, new_name)
+                if not os.path.exists(new_name_path):
+                    shutil.copy(src_file, new_name_path)
+                    write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
+                               (num, rev, eco, src_file, new_name)), result_log)
+                    break
+                ii += 1
+
+
+def debug_copy(num, rev, eco, root, file, line, dstdir, result_log, debug=False, debug_log=""):
+    src_file = os.path.join(root, file)
+    src_file_name, src_file_extension = os.path.splitext(src_file)
+    if src_file_extension == ".pdf":
+        # right now I only want to get pdf files because that is the data we have
+        # TODO pull the hardcode .pdf extension out of here and make it selectable
+        new_file_name = create_new_part_name(line) + src_file_extension  # get a new name
+        new_dst_file_name = os.path.join(dstdir, new_file_name)
+        if not os.path.exists(new_dst_file_name):
+            print("copy src:\t%s\tto:\t%s" % (src_file, new_dst_file_name))
+            write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
+                       (num, rev, eco, src_file, new_file_name)), result_log)
+        else:
+            ii = 1
+            while True:
+                new_name = os.path.join(create_new_part_name(line) + "(" + str(ii) + ")"
+                                        + src_file_extension)
+                new_name_path = os.path.join(dstdir, new_name)
+                if not os.path.exists(new_name_path):
+                    print("copy src:\t%s\tto:\t%s" % (src_file, new_name_path))
+                    write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
+                               (num, rev, eco, src_file, new_name)), result_log)
+                    break
+                ii += 1
+
+
+def iterate_over_list_create_objects(data, errorlog, dstdir, index_file, result_log, debug=False, debug_log=""):
     with open(data, "r+") as f:  # open the data file assuming it's in the right format
         total_for_counter = 1
         elapsed_time = 0
@@ -122,68 +222,26 @@ def iterate_over_list_create_objects(data, errorlog, dstdir, index_file, result_
                         for row in reader:
                             root = row[0]
                             file = row[1]
-                            #with Timer() as t:
-                            if num in file and eco in root and find_rev(file, errorlog) is not None \
-                                    and find_rev(file, errorlog) == filter_zeros_from_rev(rev) and not \
-                                    is_material_spec(file) and not is_a_bom_redline(file):  # TODO refactor this
-                                # If the file has the part number we're looking for in it, and we can extract the rev
-                                # and the rev matches what we're looking for, and it's not a mat spec or bom redline,
-                                # then we'll consider it our part number + rev combo
-                                src_file = os.path.join(root, file)
-                                src_file_name, src_file_extension = os.path.splitext(src_file)
-                                if src_file_extension == ".pdf":
-                                    # right now I only want to get pdf files because that is the data we have
-                                    # TODO pull the hardcode .pdf extension out of here and make it selectable
-                                    new_file_name = create_new_part_name(line) + src_file_extension  # get a new name
-                                    new_dst_file_name = os.path.join(dstdir, new_file_name)
-                                    if not os.path.exists(new_dst_file_name):
-                                        shutil.copy(src_file, new_dst_file_name)
-                                        write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
-                                                   (num, rev, eco, src_file, new_file_name)), result_log)
-                                    else:
-                                        ii = 1
-                                        while True:
-                                            new_name = os.path.join(create_new_part_name(line) + "(" + str(ii) + ")"
-                                                                    + src_file_extension)
-                                            new_name_path = os.path.join(dstdir, new_name)
-                                            if not os.path.exists(new_name_path):
-                                                shutil.copy(src_file, new_name_path)
-                                                write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
-                                                           (num, rev, eco, src_file, new_name)), result_log)
-                                                break
-                                            ii += 1
+                            if is_item_attachment(num, file, eco, rev, root, errorlog, debug, debug_log):
+                                if not debug:
+                                    perform_copy(num, rev, eco, root, file, line, dstdir, result_log)
+                                else:
+                                    debug_copy(num, rev, eco, root, file, line, dstdir, result_log, debug, debug_log)
 
                             elif num in file and eco in root and is_new_rev_folder(root) and not \
                                     is_material_spec(file) and not is_a_bom_redline(file):
-                                src_file = os.path.join(root, file)
-                                src_file_name, src_file_extension = os.path.splitext(src_file)
-                                if src_file_extension == ".pdf":
-                                    # right now I only want to get pdf files because that is the data we have
-                                    # TODO pull the hardcode .pdf extension out of here and make it selectable
-                                    new_file_name = create_new_part_name(line) + src_file_extension  # get a new name
-                                    new_dst_file_name = os.path.join(dstdir, new_file_name)
-                                    if not os.path.exists(new_dst_file_name):
-                                        shutil.copy(src_file, new_dst_file_name)
-                                        write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
-                                                   (num, rev, eco, src_file, new_file_name)), result_log)
-                                    else:
-                                        ii = 1
-                                        while True:
-                                            new_name = os.path.join(create_new_part_name(line) + "(" + str(ii) + ")" \
-                                                                    + src_file_extension)
-                                            new_name_path = os.path.join(dstdir, new_name)
-                                            if not os.path.exists(new_name_path):
-                                                shutil.copy(src_file, new_name_path)
-                                                write_log(("Target:\t%s-%s\ton ECO:\t%s\tfrom:\t%s\tto:\t%s" %
-                                                           (num, rev, eco, src_file, new_name)), result_log)
-                                                break
-                                            ii += 1
-                            #print("=> elasped test IF: %s s" % t.secs)
+                                if not debug:
+                                    perform_copy(num, rev, eco, root, file, line, dstdir, result_log)
+                                else:
+                                    debug_copy(num, rev, eco, root, file, line, dstdir, result_log, debug, debug_log)
+
                 else:  # line length == 0
                     write_log("line: %s" % line, errorlog, "Zero length line")
-            # print("=> elasped test loop: %s s" % t.secs)
             elapsed_time = elapsed_time + t.secs
             total_for_counter += 1
             average_time = elapsed_time/total_for_counter
             estimated_time_left = ((23667 - total_for_counter) * average_time)/60
-            print("=> average time: %s s -- estimate remaining: %s m" % (average_time, estimated_time_left))
+            if total_for_counter % 50 == 0:
+                print("=> average time: %s s -- estimate remaining: %s m" % (average_time, estimated_time_left))
+            if estimated_time_left < 1:
+                write_log("=> average time: %s s\t-- total time: %s m" % (average_time, elapsed_time), result_log)
--- app/tests/test_Iterate.py
+++ app/tests/test_Iterate.py
@@ -8,4 +8,6 @@ error_log = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/
 results_log = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/results/results_log.txt")
 dstdir = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/results")
 index_file = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/indexes/local_att_delim.txt")
-print(iterate_over_list_create_objects(data, error_log, dstdir, index_file, results_log))
+debug = True
+debug_log = os.path.normpath("c:/users/foxma/documents/github/avantiattachments/results/debug_log.txt")
+print(iterate_over_list_create_objects(data, error_log, dstdir, index_file, results_log, debug, debug_log))
